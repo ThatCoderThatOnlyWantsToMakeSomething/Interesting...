e713daf5a7234f24030b2c998133bfe894649ab9
Triton <67922012+ThatCoderThatOnlyWantsToMakeSomething@users.noreply.github.com>
2021-12-22 15:16:33 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 function myFunction() {
   var x = document.getElementById("myText").value;
   if (x === "marblex"){
-  document.getElementById("demo").innerHTML = "https://docs.google.com/document/d/1_r5qYjucV4WESTE2hUyeRHKT6aE9yiFHkgmoP0i3HCk/edit?usp=sharing";
+  document.getElementById("demo").innerHTML = "Press Control Shift I, and visit console.";
+  console.log("https://docs.google.com/document/d/1_r5qYjucV4WESTE2hUyeRHKT6aE9yiFHkgmoP0i3HCk/edit?usp=sharing");
   }
   }
