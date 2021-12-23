db9bc9e295febc832b03c75226c1f45106f84e78
Triton <67922012+ThatCoderThatOnlyWantsToMakeSomething@users.noreply.github.com>
2021-12-23 14:59:08 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function myFunction() {
   var x = document.getElementById("myText").value;
-  if (x === "marblex"){
+  if (x === "marblex ?"){
   document.getElementById("demo").innerHTML = "Press Control Shift I, and visit console.";
   console.log("https://docs.google.com/document/d/1_r5qYjucV4WESTE2hUyeRHKT6aE9yiFHkgmoP0i3HCk/edit?usp=sharing");
   }
